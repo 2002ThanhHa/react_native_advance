cc22919f61cc8b81e6ebdd3e5b5f7cf67146633a
ThanhHa <nime63526332@gmail.com>
2024-03-25 17:55:34 +0700

    cau 4 bogn bong

--- Animation/animation/Ex/Ex1.js
+++ Animation/animation/Ex/Ex1.js
@@ -1,35 +1,39 @@
-import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import React, { Component } from "react";
+import { View, Animated } from "react-native";
 
 class Bubble extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fly: new Animated.Value(800), 
+      fly: new Animated.Value(1000),
     };
   }
 
   componentDidMount() {
-    const flys =()=>{
-        const items =[];
-        for (var i=0 ;  i < 7; i++ ){
-            items.push(
-                Animated.timing(this.state.fly, {
-                    toValue: 1, 
-                    duration: 3000,
-                    useNativeDriver: false,
-                  }).start()
-            )}
-        }
-    }
+    // const flys =()=>{
+    //     const items =[];
+    //     for (var i=0 ;  i < 7; i++ ){
+    //         items.push(
+    //             Animated.timing(this.state.fly, {
+    //                 toValue: 1,
+    //                 duration: 3000,
+    //                 useNativeDriver: false,
+    //               }).start()
+    //         )}
+    //     }
+    Animated.timing(this.state.fly, {
+      toValue: 1,
+      duration: 5000,
+      useNativeDriver: false,
+    }).start();
+  }
 
-    // Animated.timing(this.state.fly, {
-    //   toValue: 1, 
-    //   duration: 3000,
-    //   useNativeDriver: false,
-    // }).start();
-// }
-  
+  //     Animated.timing(this.state.fly, {
+  //       toValue: 1,
+  //       duration: 3000,
+  //       useNativeDriver: false,
+  //     }).start();
+  // }
 
   render() {
     return (
@@ -38,17 +42,53 @@ class Bubble extends Component {
           transform: [{ translateY: this.state.fly }],
           opacity: this.state.fly.interpolate({
             inputRange: [0, 800],
-            outputRange: [0,1],
+            outputRange: [0, 1],
           }),
         }}
       >
-        
         <View
           style={{
             marginLeft: 180,
             width: 50,
             height: 50,
-            backgroundColor: 'pink',
+            backgroundColor: "pink",
+            borderRadius: 50,
+          }}
+        />
+     
+        <View
+          style={{
+            marginLeft: 180,
+            width: 50,
+            height: 50,
+            backgroundColor: "pink",
+            borderRadius: 50,
+          }}
+        />
+        <View
+          style={{
+            marginLeft: 180,
+            width: 50,
+            height: 50,
+            backgroundColor: "pink",
+            borderRadius: 50,
+          }}
+        />
+        <View
+          style={{
+            marginLeft: 180,
+            width: 50,
+            height: 50,
+            backgroundColor: "pink",
+            borderRadius: 50,
+          }}
+        />
+        <View
+          style={{
+            marginLeft: 180,
+            width: 50,
+            height: 50,
+            backgroundColor: "pink",
             borderRadius: 50,
           }}
         />
